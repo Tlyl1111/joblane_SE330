a2c3eab9e0302313ffa31a7f0dce01fa01048d63
Tlyl1111 <yenlinhtranle1111@gmail.com>
2024-05-21 18:25:06 +0700

    update standalone

--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -5,8 +5,11 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
 @Component({
   selector: 'app-sign-in', 
+  standalone: true,
+  imports: [],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 
