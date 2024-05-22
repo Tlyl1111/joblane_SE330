6b966b1059d971334c2812d0cf8a901ad5d3d7dc
Tlyl1111 <yenlinhtranle1111@gmail.com>
2024-05-22 08:38:30 +0700

    update form login

--- frontend/src/app/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/pages/sign-in/sign-in.component.ts
@@ -5,22 +5,38 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in', 
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 
 })
-export class SignInComponent {
-  email: string = '';
-  password: string = '';
+export class SignInComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor(private authService: AuthService) {}
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
 
-  onLogin(): void {
-    
+  ngOnInit(): void {
+  }
+
+  onLogin() {
+    const email = this.loginForm.get('email')!.value;
+    const password = this.loginForm.get('password')!.value;
+    if (email === 'abc@gmail.com' && password === 'abc') {
+      alert('Đăng nhập thành công!');
+    } else {
+      alert('Email hoặc mật khẩu không chính xác.');
+    }
   }
 }
