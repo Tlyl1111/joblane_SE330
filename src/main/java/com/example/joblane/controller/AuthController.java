45e0ffabba54fe509602d4427d27f955922cebac
Tlyl1111 <yenlinhtranle1111@gmail.com>
2024-05-26 22:18:16 +0700

    update sign-in

--- src/main/java/com/example/joblane/controller/AuthController.java
+++ src/main/java/com/example/joblane/controller/AuthController.java
@@ -1,24 +1,34 @@
 package com.example.joblane.controller;
 
-import com.example.joblane.entity.Users;
-import com.example.joblane.repository.UserRepository;
+import com.example.joblane.model.dto.UserLoginRequest;
+import com.example.joblane.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
-    private UserRepository usersRepository;
+    private UserService userService;
 
     @PostMapping("/login")
-    public String login(@RequestBody Users user) {
-        Users foundUser = usersRepository.findByEmailAndPassword(user.getEmail(), user.getPassword());
-        if (foundUser != null) {
-            return "Login Successful!";
-        } else {
-            return "Login Failed";
-        }
+    public ResponseEntity<String> login(@RequestBody UserLoginRequest request) {
+      boolean isValid = userService.validateUser(request.getEmail(), request.getPassword());
+      if (isValid) {
+        System.out.println("successfully");
+          return ResponseEntity.ok("Đăng nhập thành công");
+          
+      } else {
+        System.out.println("unsuccessfully");
+          return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Đăng nhập không thành công");
+      }
     }
+    
 }
