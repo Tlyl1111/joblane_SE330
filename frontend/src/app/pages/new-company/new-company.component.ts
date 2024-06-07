2f50cfde823b475ce5be32d1c7da556b0b6f72b4
nous366 <21521392@gm.uit.edu.vn>
2024-06-07 11:06:21 +0700

    new-company

---
+++ frontend/src/app/pages/new-company/new-company.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-company',
+  standalone: true,
+  templateUrl: './new-company.component.html',
+  styleUrl: './new-company.component.scss',
+})
+export class NewCompanyComponent {
+
+}
